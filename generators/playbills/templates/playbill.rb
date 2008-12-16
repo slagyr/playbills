fb23a765d5ce69967fe3971be08b8c3ecf1d7538
Micah Martin <micah@8thlight.com>
2008-12-16 17:06:28 -0600

    added missing code

--- generators/playbills/templates/playbill.rb
+++ generators/playbills/templates/playbill.rb
@@ -1,12 +1,29 @@
 class Playbill < ActiveRecord::Base
-  PLAYBILL_ROOT = File.join(Rails.root, "public", "playbill_data")
-
-
+  
+  def self.data_root
+    return ENV["PLAYBILLS_ROOT"] || "playbills"
+  end
+  
+  def data_dir
+    return File.join(Playbill.data_dir, id.to_s)
+  end
+  
+  def establish_data_dir
+    establish_dir(data_dir)
+  end
+  
   def thumbnail_path
     File.join(PLAYBILL_ROOT, id.to_s, "#{name}.png")
   end
 
   def llp_path
     File.join(PLAYBILL_ROOT, id.to_s, "#{name}.llp")
+  end
+  
+  private #################################################
+  
+  def establish_dir(dir)
+    establish_dir(File.dirname(dir)) if !File.exist?(dir)
+    Dir.mkdir(dir) if !File.exist?(dir)
   end
 end
