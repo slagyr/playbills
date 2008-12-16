55be9bcb763440e115d690689ce5c2dfd09d862d
Micah Martin <micah@8thlight.com>
2008-12-16 17:33:38 -0600

    installing

--- generators/playbills/playbills_generator.rb
+++ generators/playbills/playbills_generator.rb
@@ -3,19 +3,15 @@ class PlaybillsGenerator < Rails::Generator::Base
   def initialize(a, b)
     super(a, b)
     system "script/generate scaffold_resource Playbill name:string title:string description:text author:string version:string size:string"
-    system "rm #{File.join('app', 'models', "playbill.rb")}"
-    system "rm #{File.join('spec', 'models', "playbill.rb")}"
-    system "rm #{File.join('app', 'controllers', "playbills_controller.rb")}"
-    system "rm #{File.join('spec', 'controllers', "playbills_controller_spec.rb")}"
   end
   
   def manifest
     record do |m|
-      m.template('playbill.rb', File.join('app', 'models', "playbill.rb"))
-      m.template('playbill_spec.rb', File.join('spec', 'models', "playbill.rb"))
-      m.template('playbills_controller.rb', File.join('app', 'controllers', "playbills_controller.rb"))
-      m.template('playbills_controller_spec.rb', File.join('spec', 'controllers', "playbills_controller_spec.rb"))
-      m.template('playbill', File.join('script', 'playbill'))
+      m.template('playbill.rb', File.join('app', 'models', "playbill.rb"), :collision => :force)
+      m.template('playbill_spec.rb', File.join('spec', 'models', "playbill.rb"), :collision => :force)
+      m.template('playbills_controller.rb', File.join('app', 'controllers', "playbills_controller.rb"), :collision => :force)
+      m.template('playbills_controller_spec.rb', File.join('spec', 'controllers', "playbills_controller_spec.rb"), :collision => :force)
+      m.template('playbill', File.join('script', 'playbill'), :chmod => "0755")
     end
   end
 
