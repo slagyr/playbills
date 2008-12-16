6f5800279062079298cfbbbdd0d966333d6baf76
Micah Martin <micah@8thlight.com>
2008-12-16 17:56:27 -0600

    installing

--- example/config/routes.rb
+++ example/config/routes.rb
@@ -13,6 +13,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :playbills
 
+  map.resources :playbills
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
